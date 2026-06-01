b3842b0cccb9dffe09cd5b72b3e161edd4d61f1f
grafamadeus <grafakranime@gmail.com>
2026-06-01 11:31:28 +0600

    open

--- echo-nomad/assets/voting.js
+++ echo-nomad/assets/voting.js
@@ -13,7 +13,7 @@ import { getFirestore, doc, setDoc, getDoc, collection, onSnapshot }
 // ════════════════════════════════════
 // 🔧 НАСТРОЙКИ
 // ════════════════════════════════════
-const VOTE_OPEN  = new Date("2026-06-01T20:00:00+06:00");
+const VOTE_OPEN  = new Date("2026-06-01T00:00:00+06:00");
 const VOTE_CLOSE = new Date("2026-06-01T21:50:00+06:00");
 
 const PARTICIPANTS = [
